d74ce16c3b58bbde58322578653a5053db222a87
gustofied <gustofied@gmail.com>
2026-01-14 20:51:16 +0100

    ad

--- scratchpad/reeruun/learning_1.py
+++ scratchpad/reeruun/learning_1.py
@@ -1,7 +1,9 @@
+from rerun.components import TextLogLevel
+from rerun_bindings import spawn
 import numpy as np
 import rerun as rr
 import matplotlib.pyplot as plt
-
+import rerun.blueprint as rrb
 plt.style.use(['dark_background'])
 
 # x = np.arange(0, 10 ,1)
@@ -58,5 +60,28 @@ print("- - - - - - - - - - - - ")
 
 # ), )
 
+
+
+rr.init("Learning_1", spawn=True)
+
+rr.set_time("time", sequence=0)
+rr.log("log/status", rr.TextLog("Application started ", level=rr.TextLogLevel.INFO))
+rr.set_time("time", sequence=5)
+rr.log("log/status", rr.TextLog("A warning", level=rr.TextLogLevel.WARN))
+for i in range(10):
+    rr.set_time("time", sequence=i)
+    rr.log(
+        "log/status", rr.TextLog(f"Proccesed item {i}", level= rr.TextLogLevel.INFO)
+    )
+
+
+# build a plueprint then send it??
+
+blueprint = rrb.Blueprint(
+    rrb.TextLogView(origin="/log", name="Text Log"),
+    rrb.TimePanel(state="expanded"),
+    collapse_panels=True
+)
+rr.send_blueprint(blueprint)
 
 
