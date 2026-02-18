7b89f6c1adbe98d805490da857a4a1199334b008
gustofied <gustofied@gmail.com>
2026-02-18 09:51:39 +0100

    Squashed 'external/recognizers/' content from commit 81385eb
    git-subtree-dir: external/recognizers
    git-subtree-split: 81385eb9d293e0e47b000ecf74bd1056dfdbcbc3

---
+++ src/recognizers/__init__.py
@@ -0,0 +1,8 @@
+from recognizers import automata
+from recognizers import json_manipulata
+from recognizers import mistral_curl
+from recognizers import mistral_sdk
+
+
+def main() -> None:
+    print("Hello from recognizers!")
