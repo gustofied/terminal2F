410a363f21585f59b92da949dee1d1436312b470
gustofied <gustofied@gmail.com>
2026-02-18 09:51:28 +0100

    remove recognizers subtree for re-add

--- external/recognizers/src/recognizers/__init__.py
+++
@@ -1,8 +0,0 @@
-from recognizers import automata
-from recognizers import json_manipulata
-from recognizers import mistral_curl
-from recognizers import mistral_sdk
-
-
-def main() -> None:
-    print("Hello from recognizers!")
